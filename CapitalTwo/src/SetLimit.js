7227f0d8946884e65946ff5492f16b7d048281fd
[Phongtran1998] <hoang_phong98@me.com>
2018-02-17 21:24:24 -0500

    Add logics to SetLimit.js

--- CapitalTwo/src/SetLimit.js
+++ CapitalTwo/src/SetLimit.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Header, Card, Input, CardSection } from './Components/common';
+import { View, Text } from 'react-native';
+import { Header, Card, Input, CardSection, Button } from './Components/common';
 
 class SetLimit extends Component {
+  state = { click: false, amount: '' }
+  onbuttonPress() {
+    this.setState({ click: true });
+  }
+
+
+  renderButton() {
+    if (this.state.click) {
+      return (
+        <Text>Your limit for donation is: {this.state.amount}</Text>
+      );
+    }
+}
   render() {
     return (
       <View>
@@ -12,7 +25,14 @@ class SetLimit extends Component {
             <Input
               placeholder="Amount"
               label="Your Limit"
+              onChangeText={(amount) => this.setState({ amount })}
             />
+          </CardSection>
+          <CardSection>
+            <Button whenPressed={() => this.onbuttonPress()}>Set Your Limit</Button>
+          </CardSection>
+          <CardSection>
+            {this.renderButton()}
           </CardSection>
         </Card>
       </View>
